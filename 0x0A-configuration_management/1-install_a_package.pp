877fc17d9261e5110bd0a63af844b122c413e231
DaveOuma <davecarlosotieno@gmail.com>
2024-08-24 03:29:12 +0300

    Using puupet to install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# Installs flask, version 2.1.0.
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Ensure Python 3 and pip3 are installed 
+package {['python3', 'python3-pip']:
+ ensure => installed,
 }
+
+# Install Flask version 2.1.0 via pip3
+exc {'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => 'usr/bin/pip3 show flask | grep -q "version: 2.1.0"',
+  require => Package['python3-pip'],
+}
+
